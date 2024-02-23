0b48d86212acadcbd8752922b8104a774be974a2
은정 <djdj5267@naver.com>
2024-02-23 18:33:41 +0900

    Initial commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,28 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+    public static void main(String[] args) {
+
+//        AppConfig appConfig = new AppConfig(); <<직접 찾아왔던 과거 ㅋ
+//        MemberService memberService = appConfig.memberService();
+////        MemberService memberService = new MemberServiceImpl();
+
+        //스프링 컨테이너, Appconfig의 설정 정보 가지고 Bean에 넣어줘서 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
